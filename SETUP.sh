1164d265ec5b7cb8fa297aecf01b7f3a5fbc7dca
S I M O N. P <spau@hotmail.de>
2023-02-15 18:50:20 +0100

    Fixed bug in grpc build source

--- SETUP.sh
+++ SETUP.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 #Change every name to the one in costum-config
-#rename proto file
-#change include directories for vscode to extern grpc
+#rename proto file + remove existing compiled protofiles if existent
+#check if extern grpc should be used
+    #change include directories for vscode to extern grpc
